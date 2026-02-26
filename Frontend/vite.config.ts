b033b431a1f4186aefcde19fd7798d984461f45a
SavE <nongsafgml@gmail.com>
2026-02-26 09:12:55 +0700

    chore : update setup

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    host: true, // จำเป็นเพื่อให้ Docker เข้าถึงได้
-    port: 5173,
-    watch: {
-      usePolling: true, // บังคับให้ Vite ตรวจไฟล์บ่อยๆ (แก้ปัญหารีโหลดบน Windows)
-      interval: 100,    // เช็คทุกๆ 0.1 วินาที
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      host: true, // จำเป็นเพื่อให้ Docker เข้าถึงได้
+      port: 5173,
+      watch: {
+        usePolling: env.VITE_WATCH_USEPOLLING === 'true', // บังคับให้ Vite ตรวจไฟล์บ่อยๆ (แก้ปัญหารีโหลดบน Windows)
+        interval: 100,    // เช็คทุกๆ 0.1 วินาที
+      },
     },
-  },
+  }
 })
